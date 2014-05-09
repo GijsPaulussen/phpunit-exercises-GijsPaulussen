2654df71c99cebad5f132d4e8a34f276a6f7c285
michelangelo <dragonbe+github@gmail.com>
2014-05-09 02:43:33 +0200

    Excercise 4: Filtering and validation of model

---
+++ ex4/tests/Utexamples/Model/ProductTest.php
@@ -0,0 +1,91 @@
+<?php
+namespace Utexamples\Model;
+/**
+ * Class ProductTest
+ * @package Utexamples\Model
+ * @group Model
+ */
+class ProductTest extends \PHPUnit_Framework_TestCase
+{
+    public function goodDataProvider()
+    {
+        return array (
+            array (
+                array (
+                    'productId'   => 1,
+                    'code'        => 'TR-0295',
+                    'title'       => 'Box of 250 paperclips',
+                    'description' => 'A resealable box with 250 paperclips',
+                    'image'       => 'http://www.example.com/img/tr-0295.jpg',
+                    'price'       => 4.95,
+                    'created'     => '2012-09-23 09:12:48',
+                    'modified'    => '2012-09-23 09:12:48',
+                ),
+            ),
+            array (
+                array (
+                    'productId'   => 2,
+                    'code'        => 'TR-0666',
+                    'title'       => 'Office Warfare multi-shot canon',
+                    'description' => 'USB-powered canon with 4 barrels',
+                    'image'       => 'http://www.example.com/img/tr-0666.jpg',
+                    'price'       => 120,
+                    'created'     => '2012-09-24 08:59:22',
+                    'modified'    => '2012-09-25 10:50:31',
+                ),
+            ),
+            array (
+                array (
+                    'productId'   => 3,
+                    'code'        => '',
+                    'title'       => '',
+                    'description' => '',
+                    'image'       => '',
+                    'price'       => 0,
+                    'created'     => '2012-09-24 08:59:22',
+                    'modified'    => '2012-09-25 10:50:31',
+                ),
+            ),
+            array (
+                array (
+                    'productId'   => 2,
+                    'code'        => 'TR-0666',
+                    'title'       => 'Office Warfare multi-shot canon',
+                    'description' => 'USB-powered canon with 4 barrels',
+                    'image'       => 'http://www.example.com/img/tr-0666.jpg',
+                    'price'       => 120,
+                    'created'     => '2012-09-24 08:59:22',
+                    'modified'    => '2012-09-25 10:50:31',
+                ),
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider goodDataProvider
+     */
+    public function testProductModelCanBePopulated($data)
+    {
+        $product = new Product($data);
+
+        $this->assertTrue($product->isValid(),
+            'Expected this Product to be valid');
+        $this->assertEquals($data['productId'], $product->getProductId(),
+            'Provisioning of productId failed');
+        $this->assertEquals($data['code'], $product->getCode(),
+            'Provisioning of code failed');
+        $this->assertEquals($data['title'], $product->getTitle(),
+            'Provisioning of title failed');
+        $this->assertEquals($data['description'], $product->getDescription(),
+            'Provisioning of description failed');
+        $this->assertEquals($data['price'], $product->getPrice(),
+            'Provisioning of price failed');
+        $this->assertEquals($data['created'], $product->getCreated()->format('Y-m-d H:i:s'),
+            'Provisioning of created failed');
+        $this->assertEquals($data['modified'], $product->getModified()->format('Y-m-d H:i:s'),
+            'Provisioning of modified failed');
+        $this->assertEquals($data, $product->toArray(),
+            'Converting this model to array failed');
+
+    }
+}
