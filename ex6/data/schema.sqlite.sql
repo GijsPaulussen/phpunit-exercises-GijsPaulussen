89f78c9ec2b303adcdf639b3745d8623295e7902
michelangelo <dragonbe+github@gmail.com>
2014-05-09 03:52:42 +0200

    Adding mock objects for DB testing

---
+++ ex6/data/schema.sqlite.sql
@@ -0,0 +1,10 @@
+CREATE TABLE product (
+  productId INTEGER NOT NULL PRIMARY KEY,
+  code TEXT,
+  title TEXT,
+  description TEXT,
+  image TEXT,
+  price REAL,
+  created TEXT,
+  modified TEXT
+);
